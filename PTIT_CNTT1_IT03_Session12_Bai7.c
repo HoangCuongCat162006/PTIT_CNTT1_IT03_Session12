8ae3ff8261f6c8510cec0739be35462cf2ce6a2e
HoangCuongCat162006 <Cathoang162006@gmail.com>
2025-07-07 22:07:44 +0700

    Add files via upload

---
+++ PTIT_CNTT1_IT03_Session12_Bai7.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+void printList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d", current->data);
+        if (current->next != NULL)
+            printf("<->");
+        current = current->next;
+    }
+    printf("->NULL\n");
+}
+
+
+void sortList(struct Node* head) {
+    if (head == NULL) return;
+
+    int swapped;
+    struct Node *ptr, *lptr = NULL;
+
+    do {
+        swapped = 0;
+        ptr = head;
+        while (ptr->next != lptr) {
+            if (ptr->data > ptr->next->data) {
+                // Hoán đổi dữ liệu
+                int temp = ptr->data;
+                ptr->data = ptr->next->data;
+                ptr->next->data = temp;
+                swapped = 1;
+            }
+            ptr = ptr->next;
+        }
+        lptr = ptr;
+    } while (swapped);
+}
+
+
+int main() {
+    struct Node* node1 = createNode(5);
+    struct Node* node2 = createNode(4);
+    struct Node* node3 = createNode(3);
+    struct Node* node4 = createNode(2);
+    struct Node* node5 = createNode(1);
+    struct Node* node6 = createNode(6);
+
+    node1->next = node2;
+    node2->prev = node1;
+    node2->next = node3;
+    node3->prev = node2;
+    node3->next = node4;
+    node4->prev = node3;
+    node4->next = node5;
+    node5->prev = node4;
+    node5->next = node6;
+    node6->prev = node5;
+
+    struct Node* head = node1;
+
+    printList(head);
+    sortList(head);
+    printList(head);
+
+    return 0;
+}
